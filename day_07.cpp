d756f2ea6ed751782006f390644d5e94fc4b2317
Birgit Schreiber <bixilein.fixilein@gmail.com>
2021-12-07 21:10:33 +0100

    Improve speed for day 7 challenge

--- day_07.cpp
+++ day_07.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include "utils.h"
 #include <algorithm>
+#include <chrono>
 
 int FuelCost(int destination, int position) {
     if (destination - position < 0) {
@@ -20,39 +21,38 @@ int CrabFuelCost(int destination, int position) {
 
 int main() {
 
+    auto start = std::chrono::steady_clock::now();
+
     int challenge_id = 7;
     auto input_data = InputData(challenge_id);
 
     std::stringstream input_stream(input_data[0]);
     std::string next_crab_position;
     std::vector<int> crab_positions;
+    double mean = 0;
     while(std::getline(input_stream, next_crab_position, ',')) {
-        crab_positions.emplace_back(std::stoi(next_crab_position));
+        int crab_position = std::stoi(next_crab_position);
+        mean += crab_position;
+        crab_positions.emplace_back(crab_position);
     }
-    int left = crab_positions[std::distance(crab_positions.begin(), std::min_element(crab_positions.begin(), crab_positions.end()))];
-    int right = crab_positions[std::distance(crab_positions.begin(), std::max_element(crab_positions.begin(), crab_positions.end()))];
+    mean /= crab_positions.size();
+    std::sort(crab_positions.begin(), crab_positions.end());
+    int median = crab_positions[crab_positions.size()/2];
 
 
-    int min_fuel_cost = INT_MAX;
-    int min_crab_fuel_cost = INT_MAX;
-    for (int current_position=left; current_position<right+1; current_position++) {
-        int total_fuel_cost = 0;
-        int total_crab_fuel_cost = 0;
-        for (auto crab_position : crab_positions) {
-            total_fuel_cost += FuelCost(current_position, crab_position);
-            total_crab_fuel_cost += CrabFuelCost(current_position, crab_position);
-        }
-        if (total_fuel_cost < min_fuel_cost) {
-            min_fuel_cost = total_fuel_cost;
-        }
-        if (total_crab_fuel_cost < min_crab_fuel_cost) {
-            min_crab_fuel_cost = total_crab_fuel_cost;
-        }
+    int total_fuel_cost = 0;
+    int total_crab_fuel_cost = 0;
+    for (auto crab_position : crab_positions) {
+        total_fuel_cost += FuelCost(median, crab_position);
+        total_crab_fuel_cost += CrabFuelCost(int(mean), crab_position);
     }
+
+    auto end = std::chrono::steady_clock::now();
 
     std::cout << "\n* Results *" << std::endl;
-    std::cout << "Day " << challenge_id << " - Part 1: " << min_fuel_cost << std::endl;
-    std::cout << "Day " << challenge_id << " - Part 2: " << min_crab_fuel_cost << std::endl;
-
+    std::cout << "Day " << challenge_id << " - Part 1: " << total_fuel_cost << std::endl;
+    std::cout << "Day " << challenge_id << " - Part 2: " << total_crab_fuel_cost << std::endl;
+    std::cout << "Calculation took " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()
+                                     << " ms" << std::endl;
     return 0;
 }
